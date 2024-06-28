c7bd324a215cbe9323e59d72e5f817d8d7d6fc58
AimanK <aimankayad@gmail.com>
2024-06-28 16:47:08 -0400

    Add logout functionality using auth0

--- ecommerce-frontend/src/layout/LogoutButton.jsx
+++ ecommerce-frontend/src/layout/LogoutButton.jsx
@@ -5,7 +5,7 @@ const LogoutButton = () => {
   const { logout } = useAuth0();
 
   return (
-    <button onClick={() => logout({ logoutParams: { returnTo: "http://localhost:3000" } })}>
+    <button className="btn btn-outline-light" onClick={() => logout({ logoutParams: { returnTo: "http://localhost:3000" } })}>
       Log Out
     </button>
   );
